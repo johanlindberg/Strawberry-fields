7768ad2afa7072f97c2788a2e0b9d8d2abd800b8
Johan Lindberg <johan@pulp.se>
2011-07-30 22:03:44 +0200

    First stab at a chronological backtracking search.
    In order for this not to grow uncontrallably wild I've narrowed it down
    to a width parameter (default 3) which means it searches at most 4
    paths per choice point.

--- s2.py
+++ s2.py
@@ -9,7 +9,7 @@ import itertools
 import sys
 import time
 
-def backtracking_search(puzzle):
+def backtracking_search(puzzle, width = 3):
   """
   backtracking_search produces a solution to <puzzle>.
 
@@ -21,7 +21,50 @@ def backtracking_search(puzzle):
   71
   ...
   """
-  return puzzle
+  max, field = puzzle
+
+  solution = (sys.maxint, None)
+
+  paths = [(common.cost(field), field), ]
+  while len(paths) > 0:
+    curr_cost, field = paths.pop(0)
+
+    # figure out the current number of greenhouses
+    greenhouses = common.ids(field)
+
+    diffs = {}
+    # try each combination of greenhouses
+    for g1, g2 in itertools.combinations(greenhouses, 2):
+      # find outer bounds (left, right, top and bottom) for a greenhouse made
+      # up of g1 and g2
+      size1, p11, p12 = common.outer_bounds(g1, field)
+      size2, p21, p22 = common.outer_bounds(g2, field)
+      size3, p31, p32 = common.outer_bounds([g1, g2], field)
+
+      if size3 is not None:
+        diff = size3 - size2 - size1
+        if diff not in diffs.keys():
+          diffs[diff] = [(g1, g2),]
+        else:
+          diffs[diff].append((g1, g2))
+
+    # find the list of joins which has the least diff, perform each join
+    # in a fresh copy of field and add it to paths if cost is lower than
+    # current cost, otherwise compare cost to solution and either discard
+    # this path or replace solution.
+    if len(diffs.keys()) > 0:
+      _paths = diffs[sorted(diffs.keys())[0]]
+      for (g1, g2) in _paths[0:width]:
+        _field = common.join(g1, g2, copy.deepcopy(field))
+        if common.cost(_field) < curr_cost:
+          paths.append((common.cost(_field), _field))
+
+        if common.cost(_field) < solution[0] and \
+           len(greenhouses) <= max + 1:
+
+          solution = (common.cost(_field), _field)
+
+  return max, solution[1]
 
 def solve(filename):
   """
